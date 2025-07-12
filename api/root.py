46337d043ede700f3072e74f62e05ec69fdd7cdb
Salem Domani <salem.domani@gmail.com>
2025-07-12 23:33:54 +0300

    feat: CORS

--- api/root.py
+++ api/root.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from db.engine import create_db_and_tables
 
@@ -6,6 +7,18 @@ from .routers import documents, query
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(documents.router, prefix="/documents", tags=["documents"])
 app.include_router(query.router, prefix="/query", tags=["query"])
